3cde1211e8caf05f78cbfb6d0da6b751ac1ee0d2
Sandres92 <eastoutcity@mail.ru>
2025-12-06 08:24:01 +0000

    Create test controller

--- main.cc
+++ main.cc
@@ -3,20 +3,13 @@
 
 int main()
 {
-    std::cout << "11\n"
-              << std::flush;
-
     // Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0", 5555);
-    std::cout << "22\n"
-              << std::flush;
 
     // Load config file
     // drogon::app().loadConfigFile("../config.json");
     // drogon::app().loadConfigFile("../config.yaml");
     // Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
-    std::cout << "33\n"
-              << std::flush;
     return 0;
 }
